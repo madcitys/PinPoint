570554bb85451386fb99db280f893e3526fd6375
Bianca <biancaisabellemartinez@gmail.com>
2026-05-10 21:45:35 +0800

    Ignored unnecessary files

--- SCAN_STATUS_CYCLE_MIGRATION.sql
+++
@@ -1,5 +0,0 @@
-alter table public.parcels drop constraint if exists parcels_status_check;
-
-alter table public.parcels
-add constraint parcels_status_check
-check (status in ('Pending', 'Outbound', 'Cancelled', 'Returned', 'Double Waybill'));
